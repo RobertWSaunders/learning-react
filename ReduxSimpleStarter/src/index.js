238f3511866154fe2ba3951e9a0bd3c8d02fbe27
Robert Saunders <robertwsaunders@outlook.com>
2017-05-06 19:28:28 -0400

    Added more to readme and starting to create app

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,15 +1,15 @@
+//go find the library called react and assign it to the variable React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new component, should produce some HTML
+const App = () => {
+  //the HTML looking stuff is JSX
+  //webpack and babel take care of this
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// take the generated HTML and put it on the page
+//have to instantiate App before passing it into the DOM
+//wrapping it in JSX tags will make it into an instance
+ReactDOM.render(<App />, document.querySelector('.container'));
